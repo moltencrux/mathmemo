dbe32a654c2b08efc7422a22a659827240b3fa4e
Graham Cole <moltencrux@gmail.com>
2023-03-29 23:29:28 +0800

    Added Copy & Paste methods, context menu + cleanup
    Added various Copy & Paste functions. Renamed EquationBox to FormulaList
    and made it a QListWidget. Also cleaned up various stuff.

--- mathmemo.py
+++ mathmemo.py
@@ -2,21 +2,34 @@
 
 import sys
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import Qt, QUrl, QEvent, QSize
-from PyQt5.QtGui import QTextDocument, QPalette, QColor
+from PyQt5.QtCore import Qt, QUrl, QEvent, QSize, QItemSelection, QItemSelectionModel, QMimeData
+from PyQt5.QtGui import QTextDocument, QPalette, QColor, QCursor, QClipboard, QImage, QPainter
 from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage, QWebEngineSettings
-from PyQt5.QtSvg import QSvgWidget, QGraphicsSvgItem
+from PyQt5.QtSvg import QSvgWidget, QGraphicsSvgItem, QSvgRenderer
 from io import BytesIO
 import matplotlib.pyplot as plt
 
 from PyQt5.QtWidgets import (QWidget, QSlider, QLineEdit, QLabel, QPushButton, QScrollArea,QApplication,
-                             QHBoxLayout, QVBoxLayout, QMainWindow, QSizePolicy)
+                             QHBoxLayout, QVBoxLayout, QMainWindow, QSizePolicy, QAbstractItemView)
 
 from PyQt5 import QtWidgets, uic
 
+from pysvg.parser import parse
+
 # from PyQt5 import Qt
 # 'PyQt5.QtWebEngineWidgets.QWebEngineSettings.ShowScrollBars'
-
+'''
+void setHeight (QPlainTextEdit *ptxt, int nRows)
+{
+    QTextDocument *pdoc = ptxt->document ();
+    QFontMetrics fm (pdoc->defaultFont ());
+    QMargins margins = ptxt->contentsMargins ();
+    int nHeight = fm.lineSpacing () * nRows +
+        (pdoc->documentMargin () + ptxt->frameWidth ()) * 2 +
+        margins.top () + margins.bottom ();
+    ptxt->setFixedHeight (nHeight);
+}
+'''
 
 mathjax_code = r'\frac{{d}}{{dx}} (x^2) = 2x'
 
@@ -55,7 +68,6 @@ mathjax_v2_config = """
                      "MathZoom.js","AssistiveMML.js", "a11y/accessibility-menu.js"],
         TeX: { extensions:
           ["AMSmath.js","AMSsymbols.js","noErrors.js","noUndefined.js"]
-        }
       });
 """.replace('{', '{{').replace('}', '}}')
 
@@ -65,7 +77,11 @@ window.MathJax = {
         enableMenu: false, ignoreHtmlClass:
             'tex2jax_ignore', processHtmlClass:
             'tex2jax_process' },
-    tex: { packages: ['base', 'ams', 'noerrors', 'noundefined'] },
+    tex: { packages: ['base', 'ams', 'noerrors', 'noundefined', '+', 'color']
+           color: { padding: 5px
+                    borderWidth: 5px
+           }
+    },
     loader: { load: ['input/tex-base', 'output/svg', 'ui/menu',
                       '[tex]/require'] },
 };
@@ -82,14 +98,13 @@ page_template = """
     </script>
   </head>
   <body>
-    <div>
+    <div style="background-color: white">
       <mathjax id="mathjax-context" style="font-size:2.3em">$${context}$$</mathjax>
       <mathjax id="mathjax-container" style="font-size:2.3em">$${{formula}}$$</mathjax>
     </div>
   </body>
 </html>
 """.format(url=mathjax_url, context=context, config=mathjax_config)
-
 
 plt.rc('mathtext', fontset='cm')
 
@@ -105,29 +120,164 @@ def render_latex_as_svg(latex_formula):
     plt.close(fig)
     return svg_image
 
-class EquationBox(QWidget):
+class FormulaList(QListWidget):
     def __init__(self, parent=None, formulas=[]):
         super().__init__(parent)
-        self.setLayout(QVBoxLayout())
-        self.layout().setSpacing(10)
-        self.layout().addStretch(1)
+        #self.itemSelectionChanged.connect()
+        #self.selectionChanged.connect
+        # self.setSizeAdjustPolicy(QListWidget.SizeAdjustPolicy.AdjustToContents)
+        self.currentItemChanged.connect(lambda: print("QLW: Item Changed Signal"))
+        self.setVerticalScrollMode(QListWidget.ScrollMode.ScrollPerPixel)
+        self.setUniformItemSizes(False)
+        self.setSelectionMode(QAbstractItemView.SingleSelection)
+        # self.setStyleSheet("QListWidget:item { selection-background-color: red; }")
+        # self.setStyleSheet("QListWidget::item:selected { background-color: red; }")
+        self.setSpacing(1)
+
+        # self.setLayout(QVBoxLayout())
+        # self.layout().setSpacing(10)
+        # self.layout().addStretch(1)
         #frame = QFrame()
+        self.setViewMode(QListWidget.ListMode)
         self.views = []
         self.formulas = []
+        self.images = []
         self.page_template = page_template
         for formula in formulas:
             self.append_formula(formula)
-            
+
         # self.setAutoFillBackground(True);
         ######
-        self.setAttribute(Qt.WA_StyledBackground, True)
-        self.setStyleSheet('background-color: white;')
+        #self.setAttribute(Qt.WA_StyledBackground, True)
+        # self.setStyleSheet('background-color: white;')
+        #self.setStyleSheet("QListWidget::item:selected { background-color: red; }")
+        #self.setStyleSheet("QListWidget { background-color: white; }")
+        #self.setStyleSheet('QListView::item:selected { border : 2px solid red; background : green; }')
+        self.setStyleSheet("QListWidget"
+                                  "{"
+                                  "background : white;"
+                                  "}"
+                                  "QListWidget QScrollBar"
+                                  "{"
+                                  "background : lightblue;"
+                                  "}"
+                                  "QListView::item:selected"
+                                  "{"
+                                  "border : 8px solid red;"
+                                  "background : green;"
+                                  "}"
+                                  )
+
         # pal = self.palette()
         # pal.setColor(self.backgroundRole(), Qt.white)
         # self.setPalette(pal)
+        self.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.listContextMenuReuquested)
+
+    def listContextMenuReuquested(self, pos):
+        print('context menu requested')
+
+        pos = self.mapFromGlobal(QCursor.pos())
+        row = self.indexAt(pos).row()
+
+        menu = QMenu(self)
+        copy_svg_act = menu.addAction('Copy SVG')
+        copy_img_act = menu.addAction('Copy Image')
+        copy_eq_act = menu.addAction('Copy Equation')
+        delete_act = menu.addAction('Delete')
+        disabled = menu.addAction('Disabled')
+
+        if row < 0:
+            for action in [copy_svg_act, copy_img_act, copy_eq_act, delete_act]:
+                action.setDisabled(True)
+
+        disabled.setDisabled(True)
+
+        menu.addAction(copy_eq_act)
 
 
+        action = menu.exec_(self.mapToGlobal(pos))
+        if action == copy_svg_act:
+            self.copySvg(row)
+        elif action == copy_img_act:
+            self.copyImage(row)
+        elif action == copy_eq_act:
+            self.copyEquation(row)
+        elif action == delete_act:
+            self.deleteEquation(row)
 
+        print("Context action {} performed on: {}".format(action, row))
+
+
+    def copySvg(self, index):
+        print('copySVG called ', index)
+
+        item = self.item(index)
+        svg_widget = self.itemWidget(item)
+
+        # create a QSvgRenderer and set it to the QSvgWidget
+        svg_renderer = svg_widget.renderer()
+        # svg_widget.setSharedRenderer(svg_renderer)
+
+        # create a QMimeData object and set the SVG data
+        mime_data = QMimeData()
+        print("copySVG: image type: ", type(self.images[index]))
+        # mime_data.setData("image/svg+xml", self.images[index])
+        mime_data.setData("image/svg", self.images[index].replace(b'currentColor', b'red')) # works for Anki
+        # mime_data.setData("image/svg+xml", self.images[index].replace(b'currentColor', b'red'))
+        #mime_data.setData("image/svg+xml", b'<?xml version="1.0" encoding="utf-8" standalone="no"?> <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><!-- Created with matplotlib (http://matplotlib.org/) --><svg height="344.88pt" version="1.1" viewBox="0 0 460.8 344.88" width="460.8pt" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">')
+        #mime_data.setData("image/svg", b'<?xml version="1.0" encoding="utf-8" standalone="no"?> <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><!-- Created with matplotlib (http://matplotlib.org/) --><svg height="344.88pt" version="1.1" viewBox="0 0 460.8 344.88" width="460.8pt" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">')
+
+        # get the system clipboard and set the QMimeData object
+        clipboard = app.clipboard()
+        clipboard.setMimeData(mime_data)
+        # print('clipboard data: ', clipboard.mimeData().data())
+        if clipboard.mimeData().hasImage():
+            print('has image')
+
+
+    def copyImage(self, index):
+        # FIX: Try scaling down the image size to see if Anki likes that
+        # more.  Mozilla seems to be fine with it tho
+
+        item = self.item(index)
+        svg_widget = self.itemWidget(item)
+
+        # create a QSvgRenderer and set it to the QSvgWidget
+
+        # Render the SVG into a QImage
+        svg_widget = QSvgWidget()
+        svg_widget.load(self.images[index].replace(b'currentColor', b'red'))
+        image = QImage(svg_widget.size() * 0.5, QImage.Format_RGB666)
+
+        painter = QPainter(image)
+        #renderer = QSvgRenderer(svgWidget.renderer())
+        renderer = svg_widget.renderer()
+        renderer.render(painter)
+        painter.end()
+        app.clipboard().setImage(image)
+        print('copyImage called ', index)
+
+    def copyEquation(self, index):
+
+        formula = self.formulas[index]
+
+        app.clipboard().setText(self.formulas[index])
+        #app.clipboard().setText(formula) # this worked
+
+        print('all', self.formulas)
+        print('copyEquation called ', self.formulas[index])
+
+    def deleteEquation(self, index):
+        self.formulas.pop(index)
+        self.images.pop(index)
+        self.takeItem(index)
+
+
+        print('deleteEquation called ', index)
+
+    #def contextMenuEvent(self, a0: QtGui.QContextMenuEvent) -> None:
+    #    ...
 
     def append_formula_svg_mpl(self, formula):
         self.formulas.append(formula)
@@ -141,28 +291,49 @@ class EquationBox(QWidget):
 
     def append_formula_svg(self, formula, svg:bytes):
 
-        # QGraphicsSvgItem()
         self.formulas.append(formula)
+        self.images.append(svg)
         svg_widget = QSvgWidget()
         svg_widget.load(svg)
         svg_widget.renderer().setAspectRatioMode(Qt.KeepAspectRatio)
-        print("sfh: ", svg_widget.renderer().defaultSize().height() // 2)
+        # svg_widget.renderer().set
+        # QSvgRenderer.set
+        svg_widget.renderer().setViewBox(svg_widget.renderer().viewBox().adjusted(0, -200, 0, 200))
         svg_widget.setFixedHeight( svg_widget.renderer().defaultSize().height() // 24)
+        # effectively adds padding around the QSvgWidget
+        print("view box: ", svg_widget.renderer().viewBox())
+
+        print("sfh resized: ", svg_widget.renderer().defaultSize().height())
+        # svg_widget.setFixedHeight( svg_widget.renderer().defaultSize().height() // 24)
         policy = QSizePolicy()
         # policy.setWidthForHeight(True)
         # policy.setHorizontalPolicy(QSizePolicy.)
         policy.setVerticalPolicy(QSizePolicy.Fixed)
         # svg_widget.setSizePolicy(policy)
-        # pal = QPalette(svg_widget.palette())
-        # pal.setColor(QPalette.Window, QColor('red'))
-        # svg_widget.setPalette(pal)
+
+        # why does this have no effect?
+        pal = QPalette(svg_widget.palette())
+        pal.setColor(QPalette.Window, QColor('red'))
+        svg_widget.setPalette(pal)
         svg_widget.setAutoFillBackground(True)
+
+
+        self.setStyleSheet("QSvgWidget { background: cyan; }")
+
         print('svg sizeHint: ', svg_widget.sizeHint())
 
-        # svg.sizeHint() returns (460, 345)
         print('svg_widegt size hint: ', svg_widget.sizeHint())
-        self.layout().addWidget(svg_widget)
-        self.views.append(svg_widget)
+        item = QListWidgetItem()
+        # item.setContentsMargins(0, 5, 0, 5)  #no effect?
+        item.setSizeHint(QSize(0, svg_widget.renderer().defaultSize().height() // 24))
+        #self.setStyleSheet("QListWidgetItem:item { selection-background-color: blue; }")
+        #self.setStyleSheet("QListView::item:selected { background-color: blue; }")
+
+        self.addItem(item)
+        self.setItemWidget(item, svg_widget)
+
+        self.views.append(item)
+        self.scrollToBottom()
 
     def append_formula(self, formula):
         self.formulas.append(formula)
@@ -204,7 +375,7 @@ class MainEqWindow(QMainWindow):
         self.preview = QWebEngineView()
         self.render = QWebEngineView()
 
-        self.preview.setFixedHeight(200)
+        #self.preview.setFixedHeight(200)
         # self.preview.setAttribute(QWebEngineSettings.ShowScrollBars)
         self.preview.settings().setAttribute(QWebEngineSettings.ShowScrollBars, False)
         # self.preview.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
@@ -215,15 +386,10 @@ class MainEqWindow(QMainWindow):
         # sp.setVerticalStretch(0)
         # self.input_box.setSizePolicy(sp)
 
-        self.scroll = QScrollArea()
+        self.eq_box= FormulaList()
 
-
-        self.scroll.setWidgetResizable(True) # CRITICAL
-
-        self.eq_box= EquationBox()
-        self.scroll.setWidget(self.eq_box)  # CRITICAL
-
-        self.widget.layout().addWidget(self.scroll)
+        self.widget.layout().addStretch(1)
+        self.widget.layout().addWidget(self.eq_box)
         self.widget.layout().addWidget(self.preview)
         self.widget.layout().addWidget(self.input_box)
         self.setCentralWidget(self.widget)
@@ -268,14 +434,8 @@ class MainEqWindow(QMainWindow):
             if event.key() == Qt.Key_Return and self.input_box.hasFocus():
                 if event.modifiers() & Qt.ControlModifier:
                     # event.accept() # not sure what this was doing.  it didn't solve the \n issue
-                    print('Shift+Enter pressed')
+                    # print('Shift+Enter pressed')
                     formula_str = self.input_box.toPlainText()
-
-
-                    # view.setFixedHeight(200)
-                    # view.setAttribute(QWebEngineSettings.ShowScrollBars)
-                    # view.settings().setAttribute(QWebEngineSettings.ShowScrollBars, False)
-                    # view.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
 
                     if formula_str:
                         print('appending formula: ', formula_str)
@@ -283,34 +443,25 @@ class MainEqWindow(QMainWindow):
                         print('svg: ', self.formula_svg)
                         self.input_box.clear()
 
-                    print('template: ', self.page_template.format(formula=formula_str))
                     self.render.setHtml(self.page_template.format(formula=formula_str),
                                          QUrl('file://'))
                     return True # this seems to delete the trailing \n.. interesting
 
         return super().eventFilter(obj, event)
 
-    def queue_result(result):
-        ...
 
     def _on_load_finished(self):
-        # Extract the SVG output from the page
-        print('on load finished')
-        #self.preview.page().runJavaScript("""
-        #    document.getElementsByTagName('svg')[0].outerHTML;
-        #""", lambda result: setattr(self, 'formula_svg', result))
+        # Extract the SVG output from the page and add an XML header
+        xml_header = b'<?xml version="1.0" encoding="utf-8" standalone="no"?>'
         self.render.page().runJavaScript("""
             var mjelement = document.getElementById('mathjax-container');
             mjelement.getElementsByTagName('svg')[0].outerHTML;
-        """, lambda result: self.update_svg(result.encode()))
+        """, lambda result: self.update_svg(xml_header + result.encode()))
         print('olf svg: ', self.formula_svg)
 
-    def update_svg(self, svg):
-        # self.svg_queue.append(svg)
+    def update_svg(self, svg:bytes):
+        # add XML header
         formula = self.eq_queue.pop(0)
-        print('update_svg svg: ', svg)
-        print('svg type: ', type(svg))
-        print('update_svg formula: ', formula)
         self.eq_box.append_formula_svg(formula, svg)
 
 
